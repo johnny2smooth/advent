cb14f0575e96d996846e152f55f552bb6ac1678f
johnny2smooth <johnbp13@gmail.com>
2023-07-26 15:13:39 -0700

    revisions

--- day3/rucksackReorganization.js
+++ day3/rucksackReorganization.js
@@ -65,12 +65,12 @@ Array(...priorityString).forEach((item, i) => (priorityMap[item] = i + 1));
 
 // second problem
 
-let testString = `vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`;
+// let testString = `vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw`;
 
 // split the string into groups of three
 
@@ -99,37 +99,85 @@ CrZsJsPPZsGzwwsLwLmpwMDw`;
 //   (i + 1) % 3 === 0 ? [strArr[i - 2], strArr[i - 1], string] : null
 // )
 
-fs.readFile("input.txt", "utf-8", (err, data) => {
-  if (err) console.log(err);
-  let split = data.trim().split(/\n/);
-  let groupOfThree = [];
-  for (let i = 0; i <= split.length - 1; i += 3) {
-    groupOfThree.push([split[i], split[i + 1], split[i + 2]]);
-  }
+// fs.readFile("input.txt", "utf-8", (err, data) => {
+//   if (err) console.log(err);
+//   let split = data.trim().split(/\n/);
+//   let groupOfThree = [];
+//   for (let i = 0; i <= split.length - 1; i += 3) {
+//     groupOfThree.push([split[i], split[i + 1], split[i + 2]]);
+//   }
 
-  // find the item that appears in all bags;
-  let commonItem = [];
-  groupOfThree.forEach((group) => {
-    const uniqueItemCount = {};
-    let sets = group.map((items) => new Set([...items]));
-    sets.forEach((items) =>
-      items.forEach((item) => {
-        uniqueItemCount[item]
-          ? (uniqueItemCount[item] += 1)
-          : (uniqueItemCount[item] = 1);
-      })
-    );
+//   // find the item that appears in all bags;
+//   let commonItem = [];
+//   groupOfThree.forEach((group) => {
+//     const uniqueItemCount = {};
+//     let sets = group.map((items) => new Set([...items]));
+//     sets.forEach((items) =>
+//       items.forEach((item) => {
+//         uniqueItemCount[item]
+//           ? (uniqueItemCount[item] += 1)
+//           : (uniqueItemCount[item] = 1);
+//       })
+//     );
 
-    for (const [key, val] of Object.entries(uniqueItemCount)) {
-      if (val === 3) commonItem.push(key);
-    }
-  });
-  let total = commonItem.reduce((sum, num) => sum + priorityMap[num], 0);
-  console.log(total);
-});
+//     for (const [key, val] of Object.entries(uniqueItemCount)) {
+//       if (val === 3) commonItem.push(key);
+//     }
+//   });
+//   let total = commonItem.reduce((sum, num) => sum + priorityMap[num], 0);
+//   // console.log(total);
+// });
 
 // let split = testString.trim().split(/\n/);
 // let groupOfThree = [];
 // for (let i = 0; i <= split.length - 1; i += 3) {
 //   groupOfThree.push([split[i], split[i + 1], split[i + 2]]);
 // }
+
+// functional programming way
+
+//
+let testString = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+const testSack = "vJrwpWtwJgWrhcsFMMfFFhFp";
+
+// split the rucksack in half
+const divideRucksack = (string) => {
+  return [string.slice(0, string.length / 2), string.slice(string.length / 2)];
+};
+
+// find the duplicate
+const findDuplicate = ([firstHalf, secondHalf]) => {
+  let firstHalfSet = new Set(firstHalf);
+  return [...secondHalf].find((item) => firstHalfSet.has(item));
+};
+
+// calculate the priority
+// - 96
+const calculatePriority = (duplicate) =>
+  duplicate.charCodeAt() - (duplicate.charCodeAt() >= 97 ? 96 : 38);
+
+const sumReducer = (sum, num) => sum + num;
+
+let total = testString
+  .split(/\n/)
+  .map(divideRucksack)
+  .map(findDuplicate)
+  .map(calculatePriority)
+  .reduce(sumReducer);
+
+fs.readFile("input.txt", "utf-8", (err, data) => {
+  if (err) console.log(err);
+  let total = data
+    .split(/\n/)
+    .map(divideRucksack)
+    .map(findDuplicate)
+    .map(calculatePriority)
+    .reduce(sumReducer);
+  console.log(total);
+});
